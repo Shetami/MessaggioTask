b67718623992ea851147cb7bf5cf9d28ded36467
Shetami <konovalov.2002.we@gmail.com>
2024-07-28 13:38:34 +0300

    create template project base version

---
+++ internal/controller/http/v1/router.go
@@ -0,0 +1,10 @@
+package v1
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func NewRouter(handler *gin.Engine) {
+}
---
+++ internal/usecase/interfaces.go
@@ -0,0 +1,18 @@
+package usecase
+
+import (
+	"context"
+
+	"github.com/Shetami/messaggio/internal/entity"
+)
+
+type (
+	MessageManager interface {
+	}
+
+	MessageManagerRepo interface {
+	}
+	
+	MessageManagerWebAPI interface {
+	}
+)
